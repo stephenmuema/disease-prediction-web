aed7b05762710b8a64b89a9bb6de447421427918
electra <muemasnyamai@gmail.com>
2020-10-24 19:26:09 +0300

    made path universal

--- pages/ml.py
+++ pages/ml.py
@@ -8,8 +8,11 @@ from sklearn.preprocessing import MinMaxScaler
 
 from tensorflow.keras.models import load_model
 
+from djangoHealthAnalytics.settings import BASE_DIR
+
 
 class ML:
+    path=BASE_DIR
     location = disease = None
 
     def __init__(self, location, disease):
@@ -21,7 +24,7 @@ class ML:
         # The code also counts the disease cases
         # At the end it creates a new csv file containing the new data
         # get all data from csv file
-        data = pd.read_csv('/home/electra/code/disease-analysis/data/Diseases.csv')
+        data = pd.read_csv('{}/data/Diseases.csv'.format(self.path))
         # set date as the index
         data = data.set_index('date')
         # get disease and location specific data
@@ -38,7 +41,7 @@ class ML:
         df
 
         # Exporting the new disease and location specific data to a csv
-        df.to_csv(r'/home/electra/code/disease-analysis/data/location.csv', index=True, header=True)
+        df.to_csv(r'{}/data/location.csv'.format(self.path), index=True, header=True)
         df
 
 
@@ -46,7 +49,7 @@ class ML:
 
         plt.style.use('fivethirtyeight')
         # convert the dataframe to nparray
-        df = pd.read_csv('/home/electra/code/disease-analysis/data/location.csv')
+        df = pd.read_csv('{}/data/location.csv'.format(self.path))
         df = df.tail(100)
         data = df.filter(['Number of cases'])
         dataset = data.values
@@ -68,7 +71,7 @@ class ML:
         for i in range(60, len(train_data)):
             x_train.append(train_data[i - 60:i, 0])
             y_train.append(train_data[i, 0])
-        model = load_model('/home/electra/code/disease-analysis/data/model.h5')
+        model = load_model('{}/data/model.h5'.format(self.path))
 
         # Create the testing dataset
         test_data = scaled_data[training_data_len - 60:, :]
@@ -100,9 +103,10 @@ class ML:
         plt.plot(train['Number of cases'])
         plt.plot(valid[['Number of cases', 'predictions']])
         # plt.show()
-        plt.savefig('/home/electra/code/disease-analysis/data/{}disease.jpg'.format(user))
-
-
-ml = ML(disease='cholera', location='machakos')
-ml.generate_csv()
-ml.generate_predictions('test')
+        plt.savefig('{}/data/{}disease.jpg'.format(self.path,user))
+#
+#
+# ml = ML(disease='cholera', location='machakos')
+# ml.generate_csv()
+# ml.generate_predictions('test')
+print(BASE_DIR)
