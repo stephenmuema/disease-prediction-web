f001f99f96e7a142ff5a0111e6fc2e5f21a625fd
steve <muemasnyamai@gmail.com>
2021-02-04 12:46:11 +0300

    requirements update

---
+++ api/merger.py
@@ -0,0 +1,32 @@
+import os
+
+from djangoHealthAnalytics.settings import BASE_DIR
+
+
+def mergeCSVs():
+    import pandas as pd
+
+    import glob
+
+    path = os.path.join(BASE_DIR, 'media', 'csv')  # use your path
+
+    all_files = glob.glob(path + "/*.csv")
+
+    li = []
+
+    for filename in all_files:
+        df = pd.read_csv(filename, index_col=None, header=0)
+
+        li.append(df)
+
+    frame = pd.concat(li, axis=0, ignore_index=True)
+    frame.rename(columns={'dateprescribed': 'date', 'diagnosis': 'disease'},inplace=True)
+    frame.drop('prescription', inplace=True, axis=1)
+    frame["disease"]=frame["disease"].str.lower()
+    frame = frame[['location', 'disease', 'gender', 'date']]
+    # frame.replace()
+    # df.to_csv('my_csv.csv', mode='a', header=False)
+    frame.to_csv('{}/data/Diseases.csv'.format(BASE_DIR), mode='a', header=False)
+
+
+mergeCSVs()
--- api/views.py
+++ api/views.py
@@ -13,24 +13,3 @@ class FileList(generics.ListCreateAPIView, generics.RetrieveUpdateDestroyAPIView
     queryset = Files.objects.all()
     serializer_class = FileSerializer
 # http http://127.0.0.1:8000/api/v1/files/ 'Authorization: Token 6c7494f9eb960d9027c12b282e57f06e7b974cc7'
-def mergeCSVs():
-        import pandas as pd
-
-        import glob
-
-        path = os.path.join(BASE_DIR, 'media', 'csv')  # use your path
-
-        all_files = glob.glob(path + "/*.csv")
-
-        li = []
-
-        for filename in all_files:
-            df = pd.read_csv(filename, index_col=None, header=0)
-
-            li.append(df)
-
-        frame = pd.concat(li, axis=0, ignore_index=True)
-        frame.rename(columns={'dateprescribed': 'date', 'diagnosis': 'disease'})
-        frame.drop('prescription', inplace=True, axis=1)
-        # df.to_csv('my_csv.csv', mode='a', header=False)
-        frame.to_csv('{}/data/Diseases1.csv'.format(self.BASE_DIR), mode='a', header=False)
