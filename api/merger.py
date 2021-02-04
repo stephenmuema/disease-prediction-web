e6155b2c24b096098085c999b2bed347f26ad987
steve <muemasnyamai@gmail.com>
2021-02-04 14:45:25 +0300

    requirements update

--- api/merger.py
+++ api/merger.py
@@ -15,16 +15,18 @@ def mergeCSVs():
     li = []
 
     for filename in all_files:
+        print(filename)
         df = pd.read_csv(filename, index_col=None, header=0)
 
         li.append(df)
+        os.remove(filename)
 
     frame = pd.concat(li, axis=0, ignore_index=True)
     frame.rename(columns={'dateprescribed': 'date', 'diagnosis': 'disease'},inplace=True)
     frame.drop('prescription', inplace=True, axis=1)
     frame["disease"]=frame["disease"].str.lower()
     frame = frame[['location', 'disease', 'gender', 'date']]
-    # frame.replace()
+    frame.replace({'male':'m','female':'f'})
     # df.to_csv('my_csv.csv', mode='a', header=False)
     frame.to_csv('{}/data/Diseases.csv'.format(BASE_DIR), mode='a', header=False)
 
--- api/models.py
+++ api/models.py
@@ -1,10 +1,12 @@
+import os
+
 from django.db import models
 
 # Create your models here.
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
-from api.views import mergeCSVs
+from djangoHealthAnalytics.settings import BASE_DIR
 
 
 class Files(models.Model):
@@ -19,9 +21,31 @@ class Files(models.Model):
 
     def save(self, *args, **kwargs):
         super(Files, self).save(*args, **kwargs)
-        mergeCSVs()
+        import pandas as pd
+
+        import glob
+
+        path = os.path.join(BASE_DIR, 'media', 'csv')  # use your path
+
+        all_files = glob.glob(path + "/*.csv")
+
+        li = []
+
+        for filename in all_files:
+            print(filename)
+            df = pd.read_csv(filename, index_col=None, header=0)
+
+            li.append(df)
+            os.remove(filename)
+
+        frame = pd.concat(li, axis=0, ignore_index=True)
+        frame.rename(columns={'dateprescribed': 'date', 'diagnosis': 'disease'}, inplace=True)
+        frame.drop('prescription', inplace=True, axis=1)
+        frame["disease"] = frame["disease"].str.lower()
+        frame = frame[['location', 'disease', 'gender', 'date']]
+        frame.replace({'male': 'm', 'female': 'f'})
+        # df.to_csv('my_csv.csv', mode='a', header=False)
+        frame.to_csv('{}/data/Diseases.csv'.format(BASE_DIR), mode='a', header=False)
 
 
-@receiver(post_delete, sender=Files)
-def submission_delete(sender, instance, **kwargs):
-    instance.file.delete(False)
+
